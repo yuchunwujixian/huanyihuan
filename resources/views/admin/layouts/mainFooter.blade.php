c21f5645d3327acdbce1a403788586c74f94dbb4
yuchunwujixian <342338015@qq.com>
2019-02-11 23:12:22 +0800

    ceshi fails

--- resources/views/admin/layouts/mainFooter.blade.php
+++ resources/views/admin/layouts/mainFooter.blade.php
@@ -4,5 +4,5 @@
       Designed by AdminLTE
     </div>
     <!-- Default to the left -->
-    <strong>Copyright &copy; 2019-2020 <a href="http://www.thinkidea.com.cn" target="_blank">zbcfree 个人技术服务团队</a></strong>  All rights reserved.
+    <strong>Copyright &copy; 2019-2020 <a href="http://www.zbcfree.com.cn" target="_blank">zbcfree 个人技术服务团队</a></strong>  All rights reserved.
   </footer>
