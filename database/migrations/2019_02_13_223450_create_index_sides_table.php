3507a87aa17a701db94f4ca9ce24944471f14a1f
yuchunwujixian <342338015@qq.com>
2019-02-13 23:15:22 +0800

    轮播图表，主题表

--- database/migrations/2019_02_13_223450_create_index_sides_table.php
+++ database/migrations/2019_02_13_223450_create_index_sides_table.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * 提示消息
+ * 首页幻灯片
  */
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateSidesTable extends Migration
+class CreateIndexSidesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -15,12 +15,15 @@ class CreateSidesTable extends Migration
      */
     public function up()
     {
-        Schema::create('tip_news', function (Blueprint $table) {
+        Schema::create('index_sides', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('title')->comment('消息主题');
-            $table->string('url')->nullable()->comment('消息跳转地址');
+            $table->string('title')->comment('幻灯片标题');
+            $table->string('url')->nullable()->comment('跳转地址');
+            $table->string('img_url')->notnullable()->comment('图片地址');
             $table->tinyInteger('status')->default(1)->comment('是否显示 0不显示1显示');
             $table->tinyInteger('sort')->comment('排序');
+            $table->tinyInteger('type')->default(0)->comment('类型0 首页 1为专题');
+            $table->integer('p_id')->default(0)->comment('type为1时，对应模块id');
             $table->timestamps();
         });
     }
@@ -32,6 +35,6 @@ class CreateSidesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('tip_news');
+        Schema::dropIfExists('index_sides');
     }
 }
--- database/migrations/2019_02_13_224213_create_topic_modules_table.php
+++ database/migrations/2019_02_13_224213_create_topic_modules_table.php
@@ -1,12 +1,10 @@
 <?php
-/**
- * 提示消息
- */
+
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateSidesTable extends Migration
+class CreateTopicModulesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -15,10 +13,9 @@ class CreateSidesTable extends Migration
      */
     public function up()
     {
-        Schema::create('tip_news', function (Blueprint $table) {
+        Schema::create('topic_modules', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('title')->comment('消息主题');
-            $table->string('url')->nullable()->comment('消息跳转地址');
+            $table->string('title')->comment('主题名称');
             $table->tinyInteger('status')->default(1)->comment('是否显示 0不显示1显示');
             $table->tinyInteger('sort')->comment('排序');
             $table->timestamps();
@@ -32,6 +29,6 @@ class CreateSidesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('tip_news');
+        Schema::dropIfExists('topic_modules');
     }
 }
