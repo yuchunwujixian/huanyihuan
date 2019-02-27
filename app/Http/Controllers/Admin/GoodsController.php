2ebd71cbc79ca2ef0b03b7a0636d1f15f5ca3cad
yuchunwujixian <342338015@qq.com>
2019-02-27 19:22:08 +0800

    商品

--- app/Http/Controllers/Admin/GoodsController.php
+++ app/Http/Controllers/Admin/GoodsController.php
@@ -48,21 +48,6 @@ class GoodsController extends Controller
 
 
     /**
-     * @name create
-     * @desc  添加
-     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
-     * @since  2017/03/18
-     * @update 2017/03/18
-     */
-    public function create()
-    {
-        $this->title = '增加幻灯片';
-        $sides_type = $this->sides_type;
-        return $this->view('admin.sides.create', compact('sides_type'));
-    }
-
-
-    /**
      * @name save
      * @desc 修改或添加
      * @param Request $request
@@ -72,35 +57,16 @@ class GoodsController extends Controller
      */
     public function save(Request $request)
     {
-        $this->validate($request, [
-            'title' => 'required|min:2',
-        ]);
-        $date['title'] = $request->input('title');
-        $date['type'] = $request->input('type');
-        $date['p_id'] = $request->input('p_id', 0);
-        $date['url'] = $request->input('url');
-        $date['sort'] = $request->input('sort');
         $date['status'] = $request->input('status');
-        if ($request->has('id')) {
-            $date['id'] = $request->input('id');
-            if ($request->hasFile('file')) {//修改图片
-                $info = Sides::find($date['id']);
-                $old_img = $info['img_url'];
-                $date['img_url'] = $request->file('file')->store('sides/'.$date['type']);
-                Storage::delete($old_img);
-            }
-            $res = Sides::where('id', $date['id'])->update($date);
-            $messge = "修改";
-
-        } else {
-            $this->validate($request, [
-                'file' => 'required|image',
-            ]);
+        $date['id'] = $request->input('id');
+        if ($request->hasFile('file')) {//修改图片
+            $info = Sides::find($date['id']);
+            $old_img = $info['img_url'];
             $date['img_url'] = $request->file('file')->store('sides/'.$date['type']);
-            $tip = new Sides();
-            $res =  $tip->fill($date)->save();
-            $messge = "添加";
+            Storage::delete($old_img);
         }
+        $res = Sides::where('id', $date['id'])->update($date);
+        $messge = "修改";
         if ($res) {
             return redirect()->route('admin.sides.index')->withSuccess($messge . '成功！');
         } else {
@@ -120,7 +86,7 @@ class GoodsController extends Controller
      */
     public function update($id)
     {
-        $this->title = '编辑幻灯片';
+        $this->title = '修改商品状态';
         $data = Sides::find($id);
         if (empty($data)){
             return redirect()->route('admin.sides.index')->withErrors('参数错误');
@@ -128,39 +94,6 @@ class GoodsController extends Controller
         $sides_type = $this->sides_type;
         $p_ids = Sides::getData($data->type);
         return $this->view('admin.sides.update', compact('data', 'sides_type', 'p_ids'));
-    }
-
-
-    /**
-     * @name del
-     * @desc 删除
-     * @param $id
-     * @return $this
-     * @since  2017/03/19
-     * @update 2017/03/19
-     */
-    public function del($id)
-    {
-        $info = Sides::find($id);
-        $old_img = $info['img_url'];
-        Storage::delete($old_img);
-        $res = $info->delete($id);
-        if ($res) {
-            return redirect()->route('admin.sides.index')->withSuccess( '删除成功！');
-        } else {
-            return redirect()->route('admin.sides.index')->withErrors( '删除失败！');
-        }
-    }
-
-    /**
-     * 或得分类下类型
-     */
-    public function getSidesType(Request $request)
-    {
-        $output = ['status' => 1, 'message' => '', 'data' => []];
-        $type = $request->input('type', 0);
-        $output['data'] = Sides::getData($type);
-        return $this->tojson($output);
     }
 
 }
