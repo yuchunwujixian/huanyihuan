76c1bacdb7aaad7ff374c67d36307cdb6a6e71bb
yuchunwujixian <342338015@qq.com>
2019-03-02 18:04:31 +0800

    aa

--- resources/views/public/tips.blade.php
+++ resources/views/public/tips.blade.php
@@ -1,12 +1,14 @@
-<div class="tips">
-    <div id="carousel-ad" class="carousel slide" data-ride="carousel"  data-interval="3000">
-        <div class="carousel-inner" role="listbox">
-            @foreach($tips as $v)
-                <div class="item @if($loop->index == 0) active @endif">
-                    <div class="img-responsive">{{ $v->title }}</div>
-                </div>
-            @endforeach
+@if(!empty($tips))
+    <div class="tips">
+        <div id="carousel-ad" class="carousel slide" data-ride="carousel"  data-interval="3000">
+            <div class="carousel-inner" role="listbox">
+                @foreach($tips as $v)
+                    <div class="item @if($loop->index == 0) active @endif">
+                        <div class="img-responsive">@if($v->url)<a href="{{ $v->url }}" class="text-info">{{ $v->title }}</a>@else{{ $v->title }} @endif</div>
+                    </div>
+                @endforeach
+            </div>
         </div>
+        <i class="glyphicon glyphicon-remove tips-del"></i>
     </div>
-    <i class="glyphicon glyphicon-remove tips-del"></i>
-</div>
+@endif
