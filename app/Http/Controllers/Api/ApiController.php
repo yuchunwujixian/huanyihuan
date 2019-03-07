d5b5f7ff51289e04811b157ed02cb2d64ef04428
yuchunwujixian <342338015@qq.com>
2019-03-07 14:27:38 +0800

    首页下专题商品

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -62,17 +62,14 @@ class ApiController extends Controller
     {
         $output = ['status' => 0, 'message' => ''];
         $output['status'] = 1;
-//        $output['data'] = Topic::with(['goods' => function ($query) {
-//            $query->where('status', 1);
-//        }])->where('status', 1)->orderBy('sort', 'asc')->limit(5)->get()->toArray();
-        $output['data'] = Topic::where('status', 1)->orderBy('sort', 'asc')->limit(5)->get();
+        $topics = Topic::where('status', 1)->orderBy('sort', 'asc')->limit(5)->get();
         //所有数据都会返回，可以循环在取数据
-        foreach ($output['data'] as &$v){
-            $sql = 'select `hyh_goods`.*, `hyh_topic_goods`.`topic_id` as `pivot_topic_id`, `hyh_topic_goods`.`goods_id` as `pivot_goods_id` '.
-                'from `hyh_goods` inner join `hyh_topic_goods` on `hyh_goods`.`id` = `hyh_topic_goods`.`goods_id` where `hyh_topic_goods`.`topic_id` = '.$v->id
-                .' and `status` = 1 and `hyh_goods`.`deleted_at` is null limit 4';
-            $v->goods = DB::select($sql);
-        }
+        $topics->each(function ($topic) {
+            $topic->load(['goods' => function ($query) {
+                $query->where('status', 1)->limit(4);
+            }]);
+        });
+        $output['data'] = $topics;
         return $this->tojson($output);
     }
 }
--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -20,17 +20,13 @@ class IndexController extends Controller
         //banner
         $banners = Sides::where('status', 1)->where('type', 0)->orderBy('sort', 'asc')->get();
         //专题 取5个，然后每个再取4个
-//        $topics = Topic::with(['goods' => function ($query) {
-//            $query->where('status', 1);
-//        }])->where('status', 1)->orderBy('sort', 'asc')->limit(5)->get();
         $topics = Topic::where('status', 1)->orderBy('sort', 'asc')->limit(5)->get();
         //所有数据都会返回，可以循环再取数据
-        foreach ($topics as &$v){
-            $sql = 'select `hyh_goods`.*, `hyh_topic_goods`.`topic_id` as `pivot_topic_id`, `hyh_topic_goods`.`goods_id` as `pivot_goods_id` '.
-                'from `hyh_goods` inner join `hyh_topic_goods` on `hyh_goods`.`id` = `hyh_topic_goods`.`goods_id` where `hyh_topic_goods`.`topic_id` = '.$v->id
-                .' and `status` = 1 and `hyh_goods`.`deleted_at` is null limit 4';
-            $v->goods = DB::select($sql);
-        }
+        $topics->each(function ($topic) {
+            $topic->load(['goods' => function ($query) {
+                $query->where('status', 1)->limit(4);
+            }]);
+        });
         return $this->view('index.index', compact('banners', 'topics'));
     }
 }
