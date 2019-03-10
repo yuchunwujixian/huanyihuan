12e77ded3c96f46e240d2019412d886e49b6b178
yuchunwujixian <342338015@qq.com>
2019-03-11 02:27:52 +0800

    首页

--- resources/views/public/footer.blade.php
+++ resources/views/public/footer.blade.php
@@ -1,38 +1,71 @@
-<div id="web-btm">
-    <div class="web-btm-up">
-        <div class="footer-logo">
-            <img src="/dist/img/footer_logo.png"/>
-        </div>
-        <div class="web-btm-links">
-            <div>
-                <a href="{{route('aboutus.index')}}">关于我们</a>
-                <a href="javascript:void(0);">版权声明</a>
-                <a href="javascript:void(0);">服务协议</a>
-                <a href="javascript:void(0);">免责声明</a>
-                <a href="javascript:void(0);">权利通知</a>
-                <a href="javascript:void(0);">数据服务</a>
+<footer class="footer">
+    <div class="container">
+        <div class="row footer-top">
+            <div class="col-md-5">
+                <h4>
+                    <img src="http://assets.bootcss.com/www/assets/img/logo.png?1552040413009" onerror="this.src='/dist/img/footer_logo.png';this.onerror=null">
+                </h4>
+                <p>我们一直致力于为广大开发者提供更多的优质技术文档和辅助开发工具！</p>
+            </div>
+            <div class="col-md-6">
+                <div class="row about">
+                    <div class="col-sm-2">
+                        <h4>关于</h4>
+                        <ul class="list-unstyled">
+                            <li><a href="/about/">关于我们</a></li>
+                            <li><a href="/ad/">广告合作</a></li>
+                            <li><a href="/links/">友情链接</a></li>
+                            <li><a href="/hr/">招聘</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-sm-4">
+                        <h4>联系方式</h4>
+                        <ul class="list-unstyled">
+                            <li>
+                                <img src="/dist/img/Tel.png"/>
+                                <span>021-25458413</span>
+                            </li>
+                            <li>
+                                <img src="/dist/img/qq.png"/>
+                                <span>6582348</span>
+                            </li>
+                            <li>
+                                <img src="/dist/img/e-mail.png"/>
+                                <span>JIONGmi1235@163.com</span>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-sm-4">
+                        <h4>旗下网站</h4>
+                        <ul class="list-unstyled">
+                            <li><a href="http://www.golaravel.com/" target="_blank">Laravel中文网</a></li>
+                            <li><a href="http://www.ghostchina.com/" target="_blank">Ghost中国</a></li>
+                            <li><a href="http://www.bootcdn.cn/" target="_blank">BootCDN</a></li>
+                            <li><a href="https://pkg.phpcomposer.com/" target="_blank">Packagist中国镜像</a></li>
+                            <li><a href="https://www.return.net/" target="_blank">燃腾教育</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-sm-2">
+                        <h4>赞助商</h4>
+                        <ul class="list-unstyled">
+                            <li><a href="http://www.maoyun.tv/" target="_blank">猫云</a></li>
+                            <li><a href="https://www.jdcloud.com/" target="_blank">京东云</a></li>
+                            <li><a href="https://www.upyun.com" target="_blank">又拍云</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+            <div class="col-md-1">
+                <h4>
+                    <img src="/dist/img/erweima.png" onerror="this.src='/dist/img/footer_logo.png';this.onerror=null">
+                </h4>
+                <p>微信公众号</p>
             </div>
         </div>
-        <div class="web-btm-contact-us">
-            <p>联系我们</p>
-            <div>
-                <img src="/dist/img/Tel.png"/>
-                <span>021-25458413</span>
-            </div>
-            <div>
-                <img src="/dist/img/qq.png"/>
-                <span>6582348</span>
-            </div>
-            <div>
-                <img src="/dist/img/e-mail.png"/>
-                <span>JIONGmi1235@163.com</span>
-            </div>
-        </div>
-        <div class="weixin">
-            <span>微<br>信<br>公<br>众<br>号</span>
-            <img src="/dist/img/erweima.png"/>
+        <hr>
+        <div class="row">
+            <p class="text-center">Copyright &copy; 2016-2017 北京炯米互联有限公司 沪ICP备17004141号-1 沪公网安备 400256413845号</p>
         </div>
     </div>
-    <hr>
-    <p class="copyright">Copyright &copy; 2016-2017 北京炯米互联有限公司 沪ICP备17004141号-1 沪公网安备 400256413845号</p>
-</div>
+</footer>
